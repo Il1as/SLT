7ae5f736142d8c3ae90bdb44b4be066b087caf21
unknown <nainiayouness@gmail.com>
2021-05-26 22:30:20 +0100

    Add preprocess img

--- Api/app.py
+++ Api/app.py
@@ -1,17 +1,68 @@
 from flask import Flask, request
 import numpy as np
 import cv2
+import mediapipe as mp
 
 app = Flask(__name__)
 
 @app.route("/",methods=['POST'])
 def index():
     file = request.files['image'].read() ## byte file
-    img=preprocess(file)
+    vect_img=preprocess(file)
     return "hello world"
 
 
 def preprocess(file):
     npimg = np.fromstring(file, np.uint8)
-    img = cv2.imdecode(npimg,cv2.IMREAD_COLOR)
-    return img
+    img = cv2.imdecode(npimg, cv2.COLOR_BGR2RGB)
+    img_vect=img_to_vector(img)
+    return img_vect
+
+
+
+def img_to_vector(image):
+    mp_drawing = mp.solutions.drawing_utils
+    mp_holistic = mp.solutions.holistic
+    row=[]
+    with mp_holistic.Holistic(static_image_mode=True,model_complexity=2) as holistic:
+        # Make Detections
+        results = holistic.process(image)
+        print(results.pose_landmarks)
+        print(results.left_hand_landmarks)      
+        print(results.right_hand_landmarks)  
+        # Export coordinates
+        # If some landmarks are not visible they are replaced with Nan in the dataset
+        
+        try:
+            
+            #if no hand is in the camera, don't insert the row    
+            if(results.left_hand_landmarks==None and results.right_hand_landmarks==None):
+                raise Exception('There is no hand in the picture')    
+                
+            if(results.pose_landmarks==None):
+                pose_row=list(np.array(([np.NaN])*132).flatten())
+            else:
+                pose = results.pose_landmarks.landmark
+                pose_row = list(np.array([[landmark.x, landmark.y, landmark.z, landmark.visibility] for landmark in pose]).flatten())
+                
+            # Extract right hand landmarks
+            if(results.right_hand_landmarks==None):
+                right_row=list(np.array(([np.NaN])*84).flatten())
+            else:
+                right = results.right_hand_landmarks.landmark
+                right_row = list(np.array([[landmark.x, landmark.y, landmark.z, landmark.visibility] for landmark in right]).flatten())
+            
+            # Extract left hand landmarks
+            if(results.left_hand_landmarks==None):
+                left_row=list(np.array(([np.NaN])*84).flatten())
+            else:
+                
+                left = results.left_hand_landmarks.landmark
+                left_row = list(np.array([[landmark.x, landmark.y, landmark.z, landmark.visibility] for landmark in left]).flatten())
+            
+            # Concate rows
+            row = pose_row+right_row+left_row
+            return row
+
+        except Exception as e:
+            print("Error : ",e)
