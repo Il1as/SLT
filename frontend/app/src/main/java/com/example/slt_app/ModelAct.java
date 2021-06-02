bf518318d21c51f7167ce05eee69d1d38d366838
AYA-ELMAZHARI <ayaelmazhari@gmail.com>
2021-06-02 10:20:07 +0100

    define model_layout (toolbar,listview,button) , list_layout and add CamAct ,

--- frontend/app/src/main/java/com/example/slt_app/ModelAct.java
+++ frontend/app/src/main/java/com/example/slt_app/ModelAct.java
@@ -1,4 +1,24 @@
 package com.example.slt_app;
 
-public class ModelAct {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ModelAct extends AppCompatActivity {
+    Button butn2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.model_act);
+        butn2 = findViewById(R.id.btn_model);
+
+        butn2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                Intent relation = new Intent(getApplicationContext(),CamAct.class);
+            }
+        });
+    }
 }
